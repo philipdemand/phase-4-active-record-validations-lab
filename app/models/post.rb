0c7372add0a15f64d9f4316c0a52c8fa8e25aff7
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-08-30 11:52:25 -0400

    commit tree d691ceb12294c8d5f0b270e5963662da4c7abb51

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_title
+
+  private
+
+  def clickbait_title
+    clickbait_phrases = [
+      "Won't Believe",
+      "Secret",
+      "Top",
+      "Guess"
+    ]
+
+    unless clickbait_phrases.any? { |phrase| title&.include?(phrase) }
+      errors.add(:title, "must be clickbait-y")
+    end
+  end
+
 end
